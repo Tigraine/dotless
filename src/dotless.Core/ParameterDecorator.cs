64294e76fb1c46def67cddeecd629ea74c80caa1
Daniel Hoelbling <tigraine@tigraine.at>
2010-06-11 20:27:00 +0200

    First draft of Parameter implementation

---
+++ src/dotless.Core/ParameterDecorator.cs
@@ -0,0 +1,35 @@
+﻿namespace dotless.Core
+{
+    using System.Collections.Generic;
+    using System.Text;
+    using Parameters;
+
+    public class ParameterDecorator : ILessEngine
+    {
+        private readonly ILessEngine subEngine;
+        private readonly IParameterSource parameterSource;
+
+        public ParameterDecorator(ILessEngine subEngine, IParameterSource parameterSource)
+        {
+            this.subEngine = subEngine;
+            this.parameterSource = parameterSource;
+        }
+
+        public string TransformToCss(string source, string fileName)
+        {
+            var sb = new StringBuilder();
+            var parameters = parameterSource.GetParameters();
+            foreach (var parameter in parameters)
+            {
+                sb.AppendFormat("@{0}: {1};\n", parameter.Key, parameter.Value);
+            }
+            sb.Append(source);
+            return subEngine.TransformToCss(sb.ToString(), fileName);
+        }
+
+        public IEnumerable<string> GetImports()
+        {
+            return subEngine.GetImports();
+        }
+    }
+}
